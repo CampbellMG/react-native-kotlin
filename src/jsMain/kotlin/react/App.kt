937efae446000f7d1d65ee4b22b5522577b4e53b
Campbell <campbell@dataforce.com.au>
2018-11-08 10:10:46 +1100

    initial commit

---
+++ src/jsMain/kotlin/react/App.kt
@@ -0,0 +1,11 @@
+package react
+
+import react.native.AppRegistry
+import react.native.Platform
+import react.native.registerComponent
+import react.native.select
+
+fun main(args: Array<String>) {
+    //register app component
+}
+
